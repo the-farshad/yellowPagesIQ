c799675a17db7b60c3f708b1d71efe6abb8fb0f7
Farshad <Farshad.1991@gmail.com>
2019-10-04 05:00:34 +0300

    first relase

--- main.py
+++ main.py
@@ -7,19 +7,29 @@ preURL = "http://www.yellowpages.com.iq/search-result.php?id="
 csvFile = open('IraqCompanyInformation', 'a')
 csvWriter = csv.writer(csvFile)
 csvWriter.writerow(['','','',''])
+csvArray = [None]*100
 
-for i in range(1,10569):
+for i in range(10500,10569):
+    counter = 0
     pageGet = requests.get(preURL+str(i))
     soupReady = bs4.BeautifulSoup(pageGet.content, 'lxml')
     companyName = soupReady.find('h2')
-    print (companyName.text)
-    # companyCategory =
-    # companyAddress =
-    # companyContact =
-    # companyEmails =
-    # companyContactDetails =
-    # for companyInformation in
-
+    csvArray[counter] = companyName
+    counter +=1
+    companyCategory = soupReady.find('h5')
+    csvArray[counter] = companyCategory
+    counter +=1
+    companyInformationDetails = soupReady.select('div p span')
+    for companyDetails in companyInformationDetails:
+        csvArray[counter] = companyDetails.text
+        counter +=1
+    counter = 30
+    contactDetails = soupReady.select('div ul li span')
+    for detail in contactDetails:
+        csvArray[counter] = detail
+        counter +=1
+        print (detail.text)
+    print (csvArray)
 
 print(i)
 csvFile.close()
