bee2bed54a56a2d31340e7df94b961a1069cecff
Farshad <Farshad.1991@gmail.com>
2019-10-04 05:55:56 +0300

    some change and optimize

--- main.py
+++ main.py
@@ -4,32 +4,34 @@ import urllib.parse
 import csv
 
 preURL = "http://www.yellowpages.com.iq/search-result.php?id="
-csvFile = open('IraqCompanyInformation', 'a')
+csvFile = open('IraqCompanyInformation.csv', 'a')
 csvWriter = csv.writer(csvFile)
-csvWriter.writerow(['','','',''])
-csvArray = [None]*100
+csvArray = [None]*30
 
-for i in range(10500,10569):
+for i in range(1,10569):
     counter = 0
     pageGet = requests.get(preURL+str(i))
     soupReady = bs4.BeautifulSoup(pageGet.content, 'lxml')
+    if soupReady.select('script')[0].text == "window.location.href='index.php';":
+       continue
     companyName = soupReady.find('h2')
-    csvArray[counter] = companyName
+    csvArray[counter] = companyName.text
     counter +=1
     companyCategory = soupReady.find('h5')
-    csvArray[counter] = companyCategory
+    csvArray[counter] = companyCategory.text
     counter +=1
     companyInformationDetails = soupReady.select('div p span')
     for companyDetails in companyInformationDetails:
         csvArray[counter] = companyDetails.text
         counter +=1
-    counter = 30
+    counter = 18
     contactDetails = soupReady.select('div ul li span')
     for detail in contactDetails:
-        csvArray[counter] = detail
+        csvArray[counter] = detail.text
         counter +=1
         print (detail.text)
     print (csvArray)
+    csvWriter.writerow(csvArray)
 
 print(i)
 csvFile.close()
