8ac37b6909bee2dc9fd838b7baae6cef847a7e27
Farshad <Farshad.1991@gmail.com>
2019-10-05 21:16:41 +0300

    change some option for better readable

--- main.py
+++ main.py
@@ -6,32 +6,52 @@ import csv
 preURL = "http://www.yellowpages.com.iq/search-result.php?id="
 csvFile = open('IraqCompanyInformation.csv', 'a')
 csvWriter = csv.writer(csvFile)
-csvArray = [None]*30
-
-for i in range(1,10569):
-    counter = 0
-    pageGet = requests.get(preURL+str(i))
-    soupReady = bs4.BeautifulSoup(pageGet.content, 'lxml')
-    if soupReady.select('script')[0].text == "window.location.href='index.php';":
-       continue
-    companyName = soupReady.find('h2')
-    csvArray[counter] = companyName.text
-    counter +=1
-    companyCategory = soupReady.find('h5')
-    csvArray[counter] = companyCategory.text
-    counter +=1
-    companyInformationDetails = soupReady.select('div p span')
-    for companyDetails in companyInformationDetails:
-        csvArray[counter] = companyDetails.text
+csvTitle = [None]*30
+csvTitle [0:9]= ['Company Name', 'Category','Landmark', 'Building', 'Street', 'City', 'Country', 'Mobile', 'Mobile', 'Email']
+csvTitle [20:]= ['Company Name', 'Founded', 'Mobile', 'Phone', 'Website', 'Email', 'Views']
+csvWriter.writerow(csvTitle)
+try:
+    for i in range(1,10600):
+        csvArray = [None]*30
+        counter = 0
+        pageGet = requests.get(preURL+str(i))
+        soupReady = bs4.BeautifulSoup(pageGet.content, 'lxml')
+        if soupReady.select('script')[0].text == "window.location.href='index.php';":
+           continue
+        companyName = soupReady.find('h2')
+        csvArray[counter] = companyName.text
+        companyCategory = soupReady.find('h5')
         counter +=1
-    counter = 18
-    contactDetails = soupReady.select('div ul li span')
-    for detail in contactDetails:
-        csvArray[counter] = detail.text
+        csvArray[counter] = companyCategory.text
+        companyInformationDetails = soupReady.select('div p span')
         counter +=1
-        print (detail.text)
-    print (csvArray)
-    csvWriter.writerow(csvArray)
-
-print(i)
+        for companyDetails in companyInformationDetails:
+            companyText = str(companyDetails.text)
+            index = companyText.find(':')
+            if len(companyText) != 0 and index != -1:
+                companyText = companyText[index+1:]
+                csvArray[counter] = companyText
+                counter += 1
+            elif len(companyText) != 0:
+                csvArray[counter] = companyDetails.text
+                counter +=1
+        counter = 20
+        contactDetails = soupReady.select('div ul li span')
+        for detail in contactDetails:
+            csvArray[counter] = detail.text
+            counter +=1
+        cleanData = []
+        for value in csvArray:
+            if type(value) == str:
+                value = value.strip('')
+                if (value == '---'):
+                    value = ''
+                print(value)
+            cleanData.append(value)
+        print(cleanData)
+        csvWriter.writerow(cleanData)
+        print(i)
+except:
+    print(i)
+    csvFile.close()
 csvFile.close()
